15d94b4c5ddd44746e01bfe615e2b4b2e9eaf0a5
Ernest Bonat, Ph.D <ernest.bonat@gmail.com>
2018-10-18 14:05:46 -0700

    Update procedures_call.py

--- Week1/Code/intel_refactoring_python_code/src/procedures_call.py
+++ Week1/Code/intel_refactoring_python_code/src/procedures_call.py
@@ -1,20 +1,40 @@
 
-import procedures
+import procedure
+import config
+import sys
+
 # from procedures import *
 
-from calculations import CalculationsClass
+from calculations_class import CalculationsClass
 
 def function_1(fDegrees): 
-    Celsius = procedures.Celsius(fDegrees)
+    Celsius = procedure.Celsius(fDegrees)
     return Celsius    
 
 def function_2(fahrenheit_temperature):
-    exception_message, celsius_temperature = procedures.fahrenheit_to_celsius(fahrenheit_temperature)
+    exception_message, celsius_temperature = procedure.fahrenheit_to_celsius(fahrenheit_temperature)
     return exception_message, celsius_temperature
 
 def function_3(fahrenheit_temperature):
     calculations = CalculationsClass()
     exception_message, celsius_temperature = calculations.fahrenheit_to_celsius(fahrenheit_temperature)
+    return exception_message, celsius_temperature
+
+
+def fahrenheit_to_celsius(fahrenheit_temperature):
+    '''
+    convert fahrenheit temperature to celsius
+    :param fahrenheit_temperature: temperature in fahrenheit
+    :return 
+    1. exception_message: string exception message 
+    2. celsius_temperature: temperature in celsius
+    '''
+    celsius_temperature = None
+    exception_message = None
+    try:
+        celsius_temperature = (fahrenheit_temperature - config.NUMBER_32) * config.NUMBER_5_DIVIDED_9
+    except:
+        exception_message = sys.exc_info()[1]        
     return exception_message, celsius_temperature
 
 def main():
